d1937d5bc87a42172c92cb230cd5155d32537546
nk-rodrigues <nkrodrigues98@gmail.com>
2022-06-02 08:36:14 +0530

    June Day 2

--- Leetcode-June/867. Transpose Matrix.cpp
+++ Leetcode-June/867. Transpose Matrix.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {  
         vector<vector<int>> invMatrix (matrix[0].size(), vector<int>(matrix.size(), 0));
         
         for(int i=0; i<matrix[0].size(); i++)

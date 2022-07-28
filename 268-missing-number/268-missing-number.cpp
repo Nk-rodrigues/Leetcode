3dbf54461e01aa4318cbb637d5ea6b819c020900
Nirmalkumar Rodrigues <47662869+Nk-rodrigues@users.noreply.github.com>
2022-07-28 23:49:33 +0530

    Time: 36 ms (30.29%), Space: 18 MB (63.93%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0, n = nums.size();
+        for(int i=0; i<n; i++){
+            ans += nums[i];
+        }
+        return (n*(n+1)/2)-ans;
+    }
+};
